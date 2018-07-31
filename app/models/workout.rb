40e96d7dbc07a4f6452387ea242a8db4cbad0521
Alvaro De La Torre <alvarodltp@gmail.com>
2018-07-31 18:12:17 -0400

    made advancements of workout mvc

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,5 +1,27 @@
 class Workout < ApplicationRecord
   belongs_to :user
-  has_many :wor
+  has_many :workouts
   has_many :exercises
+
+
+  def generate_random_workout(equipment, muscle, intensity, number)
+    arr = []
+    n = muscle.count
+    n.times do |i|
+      if equipment == true
+
+        Exercise.all.select {|exercise| exercise.muscle_group == muscle}.sample(number)
+      else
+        Exercise.all.select {|exercise| exercise.muscle_group == muscle && exercise.equipment == false}.sample(number)
+      end
+    end
+  end
+
+  def muscles=(attr)
+    @muscles = attr
+  end
+
+  def muscles
+    @muscles
+  end
 end
