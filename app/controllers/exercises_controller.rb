0e00a2c0e9b7795cdad5d69145ab0fc7d3940da1
github username <github email address>
2018-07-31 16:53:07 -0400

    Exercise basic MVC

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,2 +1,50 @@
 class ExercisesController < ApplicationController
+  before_action :set_exercise, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @exercises = Exercise.all
+  end
+
+  def show
+  end
+
+  def new
+    @exercise = Exercise.new
+  end
+
+  def create
+    @exercise = Exercise.new(exercise_params)
+    if @exercise.save
+      redirect_to exercise_path(@exercise)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @exercise.update(exercise_params)
+      redirect_to exercise_path(@exercise)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @exercise.delete
+    redirect_to home_path
+  end
+
+
+  private
+
+  def set_exercise
+    @exercise = Exercise.find(params[:id])
+  end
+
+  def exercise_params
+    params.require(:exercise).permit(:name, :muscle_group, :muscle_part, :equipment, :high_intensity)
+  end
 end
