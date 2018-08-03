dcccd53c6ec27723c3e8c86216feaed7850d2a8b
github username <github email address>
2018-08-02 20:27:46 -0400

    delete all the prys

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,6 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
-      binding.pry
       flash[:alert] = "Incorrect username or password DUMBASS"
       redirect_to login_path
     end
