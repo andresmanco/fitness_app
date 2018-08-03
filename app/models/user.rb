1c8ac3195667edaa521374fa5ffd086598198d54
github username <github email address>
2018-08-03 15:17:39 -0400

    Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :workouts
   has_many :user_exercises
   has_many :exercises, through: :user_exercises
-  validates :user_name, presence: true
+  validates :user_name, :first_name, :last_name, :password, presence: true
   # validates :password_digest, presence: true, length: { minimum: 6 }
   has_secure_password
 
